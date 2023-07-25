bad8c37197e676fbf48ea6723d00ce5d55270494
CyrilPatrice <cpatrice@solocal.com>
2023-07-25 15:07:10 +0200

    macos maven

--- step.sh
+++ step.sh
@@ -5,5 +5,5 @@ THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #cd "${THIS_SCRIPTDIR}"
 
-mvn -q install:install-file -Dfile=$THIS_SCRIPTDIR/maven/kraph-v.0.7.0.jar -DpomFile=$THIS_SCRIPTDIR/maven/pom-default.xml                                        
+mvn -e install:install-file -Dfile=$THIS_SCRIPTDIR/maven/kraph-v.0.7.0.jar -DpomFile=$THIS_SCRIPTDIR/maven/pom-default.xml                                        
 kscript $THIS_SCRIPTDIR/step.kts
